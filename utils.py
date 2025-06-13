2cc7ed508f383c6b379f7c2b1a242fde2a4f5876
Dennis Mustafić <dennismstfc@gmail.com>
2025-06-13 11:58:11 +0200

    Adding png to pil converter

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import base64
 from io import BytesIO
 from PIL import Image
+from pathlib import Path
 
 def pil_to_base64(
         image: Image.Image, 
@@ -19,3 +20,14 @@ def pil_to_base64(
     image.save(buffered, format=format)
     img_str = base64.b64encode(buffered.getvalue()).decode("utf-8")
     return f"data:image/{format.lower()};base64,{img_str}"
+
+
+def png_to_pil(path: Path) -> Image.Image:
+    """
+    Convert a PNG image to a PIL image.
+    Args:
+        path: path to the PNG image
+    Returns:
+        PIL image  
+    """
+    return Image.open(path)
