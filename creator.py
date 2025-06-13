afc45026765995e2f9d92b01566eea1813a78f6b
Dennis Mustafić <dennismstfc@gmail.com>
2025-06-13 18:23:27 +0200

    Adding gpt-3o support

--- creator.py
+++ creator.py
@@ -5,8 +5,9 @@ import re
 import streamlit as st
 import pandas as pd
 
+from analyzer import FileAnalyzer
 from structures import FlashCardStruct
-from few_shot_examples import few_shot_examples
+from few_shot_examples import few_shot_examples_gpt4o, few_shot_examples_gpt3o
 from utils import pil_to_base64
 
 
@@ -33,6 +34,9 @@ class FlashCardCreator:
         self.client = openai.OpenAI(api_key=OPENAI_API_KEY)
         self.chapter = chapter
 
+        analyzer = FileAnalyzer(self.pages)
+        self.analysis = analyzer.analyze()
+
     def create_flashcards(self) -> list[FlashCardStruct]:
         """
         Create flashcards for the selected pages.
@@ -44,8 +48,11 @@ class FlashCardCreator:
         questions = []
         answers = []
 
-        for page in self.pages:
-            response = self.create_flashcards_for_page(page)
+        for idx, page in enumerate(self.pages):
+            if self.analysis[idx]['use_gpt4o']:
+                response = self.create_flashcards_for_page_gpt4o(page)
+            else:
+                response = self.create_flashcards_for_page_gpt3o(self.analysis[idx]['text'])
             # The response can contain multiple flashcards, so we need to split them
             # since they are separated by <Question> and <Answer> tags
 
@@ -58,7 +65,7 @@ class FlashCardCreator:
         return flashcards
 
 
-    def create_flashcards_for_page(self, page: PIL.Image.Image):
+    def create_flashcards_for_page_gpt4o(self, page: PIL.Image.Image):
         """
         Create flashcards for a single page.
         
@@ -72,7 +79,7 @@ class FlashCardCreator:
         img_str = pil_to_base64(page)
         
         # Create the message for GPT-4 Vision using few-shot examples
-        messages = few_shot_examples.copy()  # Start with the few-shot examples
+        messages = few_shot_examples_gpt4o.copy()  # Start with the few-shot examples
         
         # Add the current page to process
         messages.append({
@@ -101,6 +108,42 @@ class FlashCardCreator:
             # Return the raw response text which should contain <Question> and <Answer> tags
             return response.choices[0].message.content
             
+        except Exception as e:
+            st.error(f"Error creating flashcards: {str(e)}")
+            return ""
+
+    def create_flashcards_for_page_gpt3o(self, text:str):
+        """
+        Create flashcards for a single page using GPT-3.5-turbo.
+
+        Args:
+            text: str of the text to process
+
+        Returns:
+            str: String containing flashcards in <Question> and <Answer> format
+        """
+        messages = few_shot_examples_gpt3o.copy()
+        messages.append({
+            "role": "user",
+            "content": [
+                {
+                    "type": "text",
+                    "text": "Create flashcards from this text following the same format as the examples."
+                },
+                {
+                    "type": "text",
+                    "text": text
+                }
+            ]
+        })
+
+        try:
+            response = self.client.chat.completions.create(
+                model="gpt-3.5-turbo",
+                messages=messages,
+                max_tokens=1000
+            )
+            return response.choices[0].message.content
         except Exception as e:
             st.error(f"Error creating flashcards: {str(e)}")
             return ""
